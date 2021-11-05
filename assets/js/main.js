876f167992fecfc400cc23a6ee533d46bc62b3bd
rem simiyu <remsimiyu@gmail.com>
2021-11-06 00:15:53 +0300

    new updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /* MOBILE NAVBAR MENU */
 const menu_Btn = document.querySelector('.navbar .menu-btn i');
-const mobile_menu = document.querySelector('.navbar .nav-menu');
-const menu_items = document.querySelectorAll('.navbar .nav-menu li a');
+const mobile_menu = document.querySelector('.navbar .menu');
+const menu_items = document.querySelectorAll('.navbar .menu li a');
 
 menu_Btn.addEventListener('click', () =>{
     menu_Btn.classList.toggle('active');
@@ -17,45 +17,49 @@ menu_items.forEach(item => {
 /* END OF MOBILE NAVBAR MENU */
 
 /* STICKY NAVBAR MENU */
-const navbar = document.querySelector('.navbar');
-
-document.addEventListener('scroll', () =>{
-    let nav = window.scrollY;
-    
-    if(nav>50 ){
-        navbar.classList.add('sticky');
-    }else{
-        navbar.classList.remove('sticky');
-    }
-})
+window.addEventListener('scroll', function(){
+    const header = document.querySelector(".navbar");
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
 /* END OF STICKY NAVBAR MENU */
+
+/*See More, See Less Btns*/ 
+const btns = document.querySelectorAll('.cta');
+
+btns.forEach(btn => {
+    btn.addEventListener('click', () =>{
+        btn.parentNode.classList.toggle('activated');
+    });
+});
+
+btns.forEach(btn => {
+    btn.addEventListener('click', () =>{
+        if( btn.parentNode.classList.contains('activated')){
+            btn.innerHTML = "See less";
+        }else{
+            btn.innerHTML = "See more";
+        }
+    });
+});
+/*End of See More, See Less Btns*/ 
 
 /* SCROLL TO TOP BTN */
 const scrollUpBtn = document.querySelector('.scroll-up-btn');
 
-    document.addEventListener('scroll', () => {
-    let scrolled = window.scrollY;
-    if(scrolled > 500){
-        scrollUpBtn.classList.add('show');
-    }else{
-        scrollUpBtn.classList.remove('show');
-    }
+    window.addEventListener('scroll', function(){
+        scrollUpBtn.classList.toggle("active", window.scrollY > 500);
 });
 
+// scroll back to top
 scrollUpBtn.addEventListener('click', () =>{
-    document.documentElement.scrollTop =0;
-})
+    document.documentElement.scrollTop = 0;
+});
 /* END OF SCROLL TO TOP BTN */
 
 /* FOOTER COPYRIGHTS SECTION */
-const year = document.querySelector('.year');
+const year = document.getElementById('year');
 
 currentYear = new Date().getFullYear();
 newYear = new Date(`january 01 ${currentYear +1} 00:00:00`);
 year.innerText = currentYear;
 /* END OF FOOTER COPYRIGHTS SECTION */
-
-
-
-
-
